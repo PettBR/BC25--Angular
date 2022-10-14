76e9a2533a802b06dd3799f9f36ed09ba7ac7bfd
PettBR <felipe.pett@gmail.com>
2022-10-14 17:14:53 -0300

    Estudo de data-binding

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /**
+   * String Interpolation: utilizar valores que propriedades possuem para mostrá-los para o usuário
+   */
+
+  /**
+   * property binding: utilizar valores de propriedades e atribuí-los a atributos HTML
+   */
+
+  /**
+   * event binding utilizado para ouvir eventos da DOM dentro do Angular
+   */
+
+  /**
+   * Two Way data binding
+   */
+
   title = 'data-binding';
+  n1: number = 0;
+  n2: number = 0;
+
+  tipoInput: string = 'password'
+  
+  somar(x: number, y: number) {
+    return x + y
+  };
+
+  mudarTipoDoInput(): void {
+    if(this.tipoInput == 'password'){
+      this.tipoInput = 'text';
+    } else{
+      this.tipoInput = 'password';
+    }
+  };
+
+
+  
 }
